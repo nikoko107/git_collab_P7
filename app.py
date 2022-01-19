c931b80272eeb0411aa1b4e825020bccb93ef8c3
niko <nicolas.dunand@gmail.com>
2022-01-19 16:52:01 +0100

    maj slide

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ st.write('DAYSEMPLOYED : ' , float(df[df['SKIDCURR'] == input_client]["DAYSEMPLO
 #else:
  #  value_EXTSOURCE1 =float(df[df['SKIDCURR'] == input_client]["EXTSOURCE1"])
 
-slide_EXTSOURCE1 = st.slider('EXTSOURCE1', max_value= 1 , value = float(df[df['SKIDCURR'] == input_client]["EXTSOURCE1"]))
+slide_EXTSOURCE1 = st.slider('EXTSOURCE1', min_value = 0.0 , max_value= 1.0 , value = float(df[df['SKIDCURR'] == input_client]["EXTSOURCE1"]))
 st.write("EXTSOURCE1 : ", slide_EXTSOURCE1)
 
 data['EXTSOURCE1'] = slide_EXTSOURCE1
