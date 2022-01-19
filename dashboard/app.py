3a319fdd3afcea632564d6657a22bd525e759873
niko <nicolas.dunand@gmail.com>
2022-01-19 14:50:40 +0100

    correction chemin

--- dashboard/app.py
+++ dashboard/app.py
@@ -30,9 +30,9 @@ PATH = os.getcwd()+'/'
 
 
 #df = joblib.load('df_data_dash.joblib')
-df = pd.read_pickle("/content/drive/MyDrive/Openclassrooms/P7 scoring/git_collab_P7/git_collab_P7/dashboard/df_data_dash.pkl")
+df = pd.read_pickle("df_data_dash.pkl")
 #model = joblib.load('gbm.joblib')
-model = pickle.load(open(filename, 'rb'))
+model = pickle.load(open('gbm.sav', 'rb'))
 feats = [f for f in df.columns if f not in ['TARGET','SKIDCURR','index']]
 
 
