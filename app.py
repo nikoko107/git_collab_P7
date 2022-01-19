050452e71f3e5756bef74b6a28c5b803592e69b3
niko <nicolas.dunand@gmail.com>
2022-01-19 17:49:26 +0100

    flag car

--- app.py
+++ app.py
@@ -112,7 +112,13 @@ else:
 
 st.write('DAYSEMPLOYED Data : ' , float(df[df['SKIDCURR'] == input_client]["DAYSEMPLOYED"]))
 
-st.write('own car Data : ' , float(df[df['SKIDCURR'] == input_client]["FLAGOWNCAR"]))
+st.write('own car Data : ' , df[df['SKIDCURR'] == input_client]["FLAGOWNCAR"])
+
+box_EXTSOURCE4 = st.checkbox('Car ' , value = df[df['SKIDCURR'] == input_client]["FLAGOWNCAR"].bool())
+if box_EXTSOURCE4:
+    data['FLAGOWNCAR'] = 1.0
+else:
+    data['FLAGOWNCAR'] = 0.0
 
 
 score_sk = float(model.predict(data)) #calcule du score en dynamique
@@ -163,19 +169,19 @@ else:
     fig_4.add_vline(x=float(df[df['SKIDCURR'] == input_client]["EXTSOURCE3"]), line_color="black")
 st.plotly_chart(fig_4, use_container_width=True)   
 
-fig_4 = px.histogram(df,x="DAYSEMPLOYED",barmode="group",histnorm='percent')
+fig_5 = px.histogram(df,x="DAYSEMPLOYED",barmode="group",histnorm='percent')
 if df[df['SKIDCURR'] == input_client]["DAYSEMPLOYED"].isnull().bool():
-    fig_4.add_annotation(dict(font=dict(color='red',size=20),text="Pas de donnée pour ce client",
-                  xref="paper", yref="paper" ))
-else:
-    fig_4.add_vline(x=float(df[df['SKIDCURR'] == input_client]["DAYSEMPLOYED"]), line_color="black")
-st.plotly_chart(fig_4, use_container_width=True)   
-
-
-fig_5 = px.histogram(df,x="FLAGOWNCAR",barmode="group",histnorm='percent')
-if df[df['SKIDCURR'] == input_client]["FLAGOWNCAR"].isnull().bool():
     fig_5.add_annotation(dict(font=dict(color='red',size=20),text="Pas de donnée pour ce client",
                   xref="paper", yref="paper" ))
 else:
-    fig_5.add_vline(x=float(df[df['SKIDCURR'] == input_client]["FLAGOWNCAR"]), line_color="black")
-st.plotly_chart(fig_4, use_container_width=True)   
+    fig_5.add_vline(x=float(df[df['SKIDCURR'] == input_client]["DAYSEMPLOYED"]), line_color="black")
+st.plotly_chart(fig_5, use_container_width=True)   
+
+
+fig_6 = px.histogram(df,x="FLAGOWNCAR",barmode="group",histnorm='percent')
+if df[df['SKIDCURR'] == input_client]["FLAGOWNCAR"].isnull().bool():
+    fig_6.add_annotation(dict(font=dict(color='red',size=20),text="Pas de donnée pour ce client",
+                  xref="paper", yref="paper" ))
+else:
+    fig_6.add_vline(x=float(df[df['SKIDCURR'] == input_client]["FLAGOWNCAR"]), line_color="black")
+st.plotly_chart(fig_6, use_container_width=True)   
